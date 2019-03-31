b9371580fdadb33006d365ad0dfc648892ad07d5
Joseph Palmer <palmer-2@hotmail.co.uk>
2019-03-31 20:06:29 +0100

    Added project proposal

---
+++ Project/Code/CreateProposal.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+# Author: Joseph Palmer <joseph.palmer18@imperial.ac.uk>
+# Script: CreateProposal.sh
+# Desc: Compiles the Project proposal tex file
+# Arguments: None
+# Date: Mar-2019
+
+./CompileLatex.sh ../WriteUp/project_proposal.tex
